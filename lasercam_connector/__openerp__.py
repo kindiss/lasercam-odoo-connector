3618d56d90606d9a7b5e02255e82c572687cbbc8
UCASE MB <kindiss@gmail.com>
2026-09-14 07:21:30 +0300

    5.1.0: new products copy the template BOM, so the laser work center is copied from the template (cost/hour, efficiency, calendar inherited)

--- lasercam_connector/__openerp__.py
+++ lasercam_connector/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'LaserCAM Connector',
-    'version': '16.0.5.0.1',
+    'version': '16.0.5.1.0',
     'category': 'Manufacturing',
     'summary': 'Export BOM data to LaserCAM nesting and import corrected quantities/times back',
     'description': """
